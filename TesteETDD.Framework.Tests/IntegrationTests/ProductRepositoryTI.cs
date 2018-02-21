e1d149a3a4fdb775e5965b72a82f697f9ff8c1f1
logonrm <logonrm@fiap.com.br>
2018-02-21 20:57:08 -0300

    Instalaçaõ do Moq

---
+++ TesteETDD.Framework.Tests/IntegrationTests/ProductRepositoryTI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using TesteETDD.Framework.Repositories;
+using TesteETDD.Framework.Repository.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TesteETDD.Framework.Tests.IntegrationTests
+{
+    [TestClass]
+    public class ProductRepositoryTI
+    {
+        private ProductRepository _productRepository;
+
+        [TestMethod]
+        public void Test_Insert_New_Product()
+        {
+            var product = new Product();
+            product.Name = "Nome do produto";
+            product.Description = "Descrição do produto";
+            product.Status = true;
+
+            var newProduct = _productRepository.Insert(product);
+
+            Assert.IsTrue(Guid.TryParse(product.ProductId.ToString(), out _));
+        }
+    }
+}
