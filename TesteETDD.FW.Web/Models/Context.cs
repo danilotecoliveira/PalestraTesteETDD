5d2ad4720292e79ccb9b93eb4853bc21b4a532c1
Danilo Oliveira <danilotecoliveira@yahoo.com.br>
2018-03-03 00:28:23 -0300

    Criação da tabela Products

--- TesteETDD.FW.Web/Models/Context.cs
+++ TesteETDD.FW.Web/Models/Context.cs
@@ -4,7 +4,7 @@ namespace TesteETDD.FW.Web.Models
 {
     public class Context : DbContext
     {
-        public Context() : base("Server=tcp:fiap11net.database.windows.net,1433;Initial Catalog=testeetdd;Persist Security Info=False;User ID={your_username};Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
+        public Context() : base(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\Projects\PalestraTesteETDD\DB\dbtestetdd.mdf;Integrated Security=True;Connect Timeout=30")
         {
 
         }
